842daf595a2fcdadee44c24624b3a0ab0bb4a44c
Anastasiia Posliednicheko <anastasia29po@gmail.com>
2025-05-18 13:20:24 +0200

    update jsx file MP

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,27 +1,77 @@
-import { useState} from "react";
+import {useEffect, useState} from "react";
+import { useSearchParams } from "react-router-dom";
 import {searchMovies} from "../../services/api";
 import MovieList from "../../components/MovieList/MovieList";
 import css from "./MoviesPage.module.css";
 
 
 export default function MoviesPage() {
-    const [query, setQuery] = useState("");
+    const [searchParams, setSearchParams] = useSearchParams();
     const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [isEmpty, setIsEmpty] = useState(false);
+    
+    
+    const query = searchParams.get("query") || "";
+
+
+    useEffect(() => {
+    if (!query) return;
+
+    setLoading(true);
+    setError(null);
+    setIsEmpty(false);
+
+    searchMovies(query)
+      .then((data) => {
+        if (data.length === 0) {
+          setIsEmpty(true);
+          setMovies([]);
+        } else {
+          setMovies(data);
+        }
+      })
+      .catch((err) => {
+        setError("Something went wrong. Please try again.");
+        console.error("Search failed:", err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+    }, [query]);
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        searchMovies(query).then(setMovies);
+        const form = e.currentTarget;
+        const inputValue = form.elements.query.value.trim();
+        if(inputValue === "") return;
+        setSearchParams({query: inputValue});
     };
 
-    return (
-        <div className={css.container}>
-            <form onSubmit={handleSubmit} className={css.form}>
-                <input className={css.input} value={query} onChange={(e) => setQuery(e.target.value)}/>
-                <button type="submit" className={css.button}>Search</button>
-            </form>
-            <MovieList movies={movies}/>
-        </div>
-    );
+     return (
+    <div className={css.container}>
+      <form onSubmit={handleSubmit} className={css.form}>
+        <input
+          className={css.input}
+          name="query"
+          defaultValue={query}
+          placeholder="Search movies..."
+        />
+        <button type="submit" className={css.button}>Search</button>
+      </form>
+
+      {loading && <p className={`${css.message} ${css.loading}`}>Loading...</p>}
+      {error && <p className={`${css.message} ${css.error}`}>{error}</p>}
+      {isEmpty && !loading && !error && (
+        <p className={`${css.message} ${css.empty}`}>
+           No results found for "{query}"
+        </p>
+       )}
+       {!loading && !error && movies.length > 0 && <MovieList movies={movies} />}
+    </div>
+  );
 }
 
 
